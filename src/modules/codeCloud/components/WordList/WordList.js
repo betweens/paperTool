59bb786362854ed50eee6647213828e69905105b
sunhe823 <sunhe823@lu.com>
2017-06-09 14:53:07 +0800

    单词列表页，添加当前论文的评论

--- src/modules/codeCloud/components/WordList/WordList.js
+++ src/modules/codeCloud/components/WordList/WordList.js
@@ -51,6 +51,87 @@ class WordList extends Component {
     return (<div className="wordList">
 
       <ul>{li}</ul>
+
+
+
+  <div className="comments-container">
+    <h1>论文评论 <a href="http://creaticode.com"></a></h1>
+
+    <ul id="comments-list" className="comments-list">
+      <li>
+        <div className="comment-main-level">
+
+          <div className="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_1_zps8e1c80cd.jpg" alt="" /></div>
+
+          <div className="comment-box">
+            <div className="comment-head">
+              <h6 className="comment-name by-author"><a href="http://creaticode.com/blog">金融</a></h6>
+              <span>hace 20 minutos</span>
+              <i className="fa fa-reply"></i>
+              <i className="fa fa-heart"></i>
+            </div>
+            <div className="comment-content">
+              J金融论文
+            </div>
+          </div>
+        </div>
+
+        <ul className="comments-list reply-list">
+          <li>
+  
+            <div className="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_2_zps7de12f8b.jpg" alt="" /></div>
+        
+            <div className="comment-box">
+              <div className="comment-head">
+                <h6 className="comment-name"><a href="http://creaticode.com/blog">Lorena Rojero</a></h6>
+                <span>hace 10 minutos</span>
+                <i className="fa fa-reply"></i>
+                <i className="fa fa-heart"></i>
+              </div>
+              <div className="comment-content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+              </div>
+            </div>
+          </li>
+
+          <li>
+
+            <div className="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_1_zps8e1c80cd.jpg" alt="" /></div>
+
+            <div className="comment-box">
+              <div className="comment-head">
+                <h6 className="comment-name by-author"><a href="http://creaticode.com/blog">Agustin Ortiz</a></h6>
+                <span>hace 10 minutos</span>
+                <i className="fa fa-reply"></i>
+                <i className="fa fa-heart"></i>
+              </div>
+              <div className="comment-content">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+              </div>
+            </div>
+          </li>
+        </ul>
+      </li>
+
+      <li>
+        <div className="comment-main-level">
+
+          <div className="comment-avatar"><img src="http://i9.photobucket.com/albums/a88/creaticode/avatar_2_zps7de12f8b.jpg" alt="" /></div>
+          <div className="comment-box">
+            <div className="comment-head">
+              <h6 className="comment-name"><a href="http://creaticode.com/blog">Lorena Rojero</a></h6>
+              <span>hace 10 minutos</span>
+              <i className="fa fa-reply"></i>
+              <i className="fa fa-heart"></i>
+            </div>
+            <div className="comment-content">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Velit omnis animi et iure laudantium vitae, praesentium optio, sapiente distinctio illo?
+            </div>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div>
     </div>);
   }
 }
