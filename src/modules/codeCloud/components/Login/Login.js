a8c98dbc83fdb961d5d009248516384de3774aff
sunhe823 <sunhe823@lu.com>
2017-06-09 14:32:31 +0800

    登录注册页面

--- src/modules/codeCloud/components/Login/Login.js
+++ src/modules/codeCloud/components/Login/Login.js
@@ -55,9 +55,42 @@ class Login extends Component {
 
   render() {
     return (<div className="Login">
-       <p><span>用户名:</span><input type="number" placeholder="输入手机号" onChange={this.userNmaehandleChange} /></p>
-       <p><span>密码:</span><input type="password" placeholder="输入密码" onChange={this.pwdhandleChange} /></p>
-       <div className="login-btn"><button type="button" onClick={this.loginFn}>登录</button><b type="button">注册</b></div>
+       {/*<p><span>用户名:</span><input type="number" placeholder="输入手机号" onChange={this.userNmaehandleChange} /></p>
+              <p><span>密码:</span><input type="password" placeholder="输入密码" onChange={this.pwdhandleChange} /></p>
+              <div className="login-btn"><button type="button" onClick={this.loginFn}>登录</button><b type="button">注册</b></div>*/}
+    
+    <div className="container">
+
+      <ul className="tabs">
+        <li><a href="#" id="1">登录</a></li>
+        <li><a href="#" id="2">注册</a></li>
+      </ul>
+
+
+      <div className="form">
+        <div className="input-container">
+           <div className="input">
+              <input className="main-form" type="text" name="usrname" placeholder=" 用户名" onChange={this.userNmaehandleChange}/>
+           </div>
+
+          <div className="input">
+              <input className="main-form" type="password" name="password" placeholder=" 密码" onChange={this.pwdhandleChange}/>
+          </div>
+
+          <div className="input">
+              <input className="main-form btn" type="button" name="usrname" value = "登录" onClick={this.loginFn}/>
+          </div>
+
+          <div className="input">
+            <label className="pull-right">
+              <a href="#" id="3" className="forget-password">忘记密码 ?</a>
+            </label>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
     </div>);
   }
 }
--- src/modules/codeCloud/components/Register/Register.js
+++ src/modules/codeCloud/components/Register/Register.js
@@ -75,11 +75,53 @@ class Register extends Component {
   }
   render() {
     return (<div className="Register">
-       <p><span>用户名:</span><input type="text" value={this.state.username}  onChange={this.userNmaehandleChange} /></p>
+{/*       <p><span>用户名:</span><input type="text" value={this.state.username}  onChange={this.userNmaehandleChange} /></p>
        <p><span>手机号:</span><input type="number" onChange={this.phonehandleChange} /></p>
        <p><span>密码:</span><input type="password" onChange={this.pwdhandleChange} /></p>
        <p><span>密码确认:</span><input type="password" onChange={this.rePwdhandleChange} /></p>
        <div className="login-btn"><button type="button" onClick={this.registerFn}>注册</button><b>已有帐号登录</b></div>
+*/}
+    <div className="container">
+
+      <ul className="tabs">
+        <li><a href="#" id="1">登录</a></li>
+        <li><a href="#" id="2">注册</a></li>
+      </ul>
+
+
+      <div className="form">
+        <div className="input-container">
+           <div className="input">
+              <input className="main-form" type="text" name="usrname" placeholder=" 用户名"  value={this.state.username}  onChange={this.userNmaehandleChange}/>
+           </div>
+
+           <div className="input">
+              <input className="main-form" type="text" name="usrname" placeholder=" 手机号"  onChange={this.phonehandleChange}/>
+           </div>
+
+          <div className="input">
+              <input className="main-form" type="password" name="password" placeholder=" 密码" onChange={this.pwdhandleChange}/>
+          </div>
+
+          <div className="input">
+              <input className="main-form" type="password" name="password" placeholder=" 确认密码" onChange={this.rePwdhandleChange}/>
+          </div>
+
+          <div className="input">
+              <input className="main-form btn" type="button" name="usrname" value = "注册" onClick={this.registerFn}/>
+          </div>
+
+          <div className="input">
+            <label className="pull-right">
+              <a href="#" id="3" className="forget-password">已有账号 ?</a>
+            </label>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+
     </div>);
   }
 }
