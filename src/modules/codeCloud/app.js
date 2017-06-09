7febabdade5d8287323f42117c087f899e4588ee
bjsunhe <bjsunhe@sina.com>
2017-06-09 23:49:15 +0800

    添加注释

--- src/modules/codeCloud/app.js
+++ src/modules/codeCloud/app.js
@@ -10,6 +10,9 @@ import Login from './components/Login';
 import Register from './components/Register';
 import MyAccount from './components/MyAccount';
 import WordList from './components/WordList';
+//说明：login 和 register之后，都进入myAccount,myAccount查wordLists表，并关联File表，列出当前用户的所有wordList和每个wordList的PDF链接
+//点击相应的单词表，进入wordList页面，列出每个wordList的所有单词
+//在myAccount页面点击上传，将PDF存储起来，将返回的wordList存入wordLists表，并和File建立关联，>>>>跳转到wordList页面，列出当前wordList的所有单词
 const BasicExample = () => (
   <Router>
     <div className="container-fluid">
